0bbda69dee6886321f270fed79a8d3fae1588bbd
kaa55500 <kaa55500@users.noreply.github.com>
2026-09-22 22:03:45 +0400

    deploy: эталоны Ц4 15/15, махи В1 в кроссовере

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VERSION и ASSETS генерируются: node tools/build-sw.mjs. Руками не править.
-const VERSION = 'v587af15f9765';
+const VERSION = 'vafe5582b87af';
 const ASSETS = [
   '.',
   'analytics.js',
